05e75d37de388a4bf7efc69feedae2ad3120506a
azpuser02 <intern-202010-02@azure-plus.co.jp>
2022-03-17 16:52:46 +0900

    LoginServletの修正を行いました。

--- azps_kintai/src/servlet/LoginServlet.java
+++ azps_kintai/src/servlet/LoginServlet.java
@@ -10,52 +10,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.AccountBeans;
+import model.Login;
+import model.LoginLogic;
+
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// フォワード
+		// 繝輔か繝ｯ繝ｼ繝�
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// リクエストパラメータの取得
 		request.setCharacterEncoding("UTF-8");
-		String userMail = request.getParameter("userMail");
+
+		String emp_Id = request.getParameter("emp_Id");
 		String pass = request.getParameter("pass");
-		String userNickname = request.getParameter("userNickname");
-		// ログイン処理の実行
-		Account loginAccount = new Account(userMail, pass,userNickname);
+
+		Login logic = new Login(emp_Id, pass);
 		LoginLogic bo = new LoginLogic();
-		Account account = bo.execute(loginAccount);/* （右辺が実行）、（左辺に代入） */
+		AccountBeans loginAccount = bo.execute(logic);
 
-		// ログイン処理の成否によって処理を分岐
-		if (account != null) {// ログイン成功時、
-			Account acc = new Account(account.getUserId(), account.getUserMail(), account.getPass(),account.getUserNickname());
-			// セッションスコープに保存されたユーザーメールを保存
+		// 繝ｭ繧ｰ繧､繝ｳ蜃ｦ逅�縺ｮ謌仙凄縺ｫ繧医▲縺ｦ蜃ｦ逅�繧貞�蟯�
+		if (loginAccount != null) {// 繝ｭ繧ｰ繧､繝ｳ謌仙粥譎ゅ��
 			HttpSession session = request.getSession();
-
-			session.setAttribute("loginAccount", acc);
-
-			GetRestaurantLogic getRestaurantLogic = new GetRestaurantLogic();
-			List<Restaurant> resList = getRestaurantLogic.findByAll();
-			session.setAttribute("resList", resList);
-
-			// フォワード
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");/* */
+			session.setAttribute("loginAccount", loginAccount);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 			dispatcher.forward(request, response);
-		} else {// ログイン失敗時
-
-			// リダイレクト
+		} else {
 			response.sendRedirect("/azps_kintai/LoginServlet");
 		}
-
 
 	}
 
